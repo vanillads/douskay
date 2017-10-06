fa093ce064b1f15f65bf8241b1c8ba5f2645de25
sandrine zecler <sandrine.zecler@efficiencesoftware.fr>
2017-10-06 13:46:04 -0400

    suite

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,2 +1,19 @@
-//= require foundation
-$(document).foundation();
+  document.getElementById('hunt-form').addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    var firstname = evt.target.elements.firstname.value;
+    var lastname = evt.target.elements.lastname.value;
+    var greeting = 'Hello';
+    if (firstname || lastname) {
+      greeting += ', ';
+      if (firstname && lastname) {
+        greeting += firstname + ' ' + lastname;
+      } else if (lastname) {
+        greeting += 'Mx. ' + lastname;
+      } else {
+        greeting += firstname;
+      }
+    }
+    greeting += '!';
+
+    document.getElementById('greeting').textContent = greeting;
+  });
