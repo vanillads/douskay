49018701ade6fbb4d5f8c5098ad1c5e090e91fca
sandrine zecler <sandrine.zecler@efficiencesoftware.fr>
2017-10-09 16:12:12 -0400

    next

--- Gemfile
+++ Gemfile
@@ -12,17 +12,15 @@ gem "jekyll", "3.5.1"
 
 gem 'font-awesome-sass'
 
-gem 'jekyll-sitemap'
-
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
 # gem "github-pages", group: :jekyll_plugins
 
 # If you have any plugins, put them here!
 group :jekyll_plugins do
-   gem "jekyll-feed", "~> 0.6"
-   gem "jekyll-assets"
+   gem "jekyll-sitemap"
    gem 'jekyll-seo-tag'
+   gem "jekyll-assets"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,19 +1,20 @@
-  document.getElementById('hunt-form').addEventListener('submit', function(evt) {
-    evt.preventDefault();
-    var firstname = evt.target.elements.firstname.value;
-    var lastname = evt.target.elements.lastname.value;
-    var greeting = 'Hello';
-    if (firstname || lastname) {
-      greeting += ', ';
-      if (firstname && lastname) {
-        greeting += firstname + ' ' + lastname;
-      } else if (lastname) {
-        greeting += 'Mx. ' + lastname;
-      } else {
-        greeting += firstname;
-      }
+window.mdc.autoInit();
+document.getElementById('hunt-form').addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  var firstname = evt.target.elements.prenom.value;
+  var lastname = evt.target.elements.nom.value;
+  var greeting = 'Hello';
+  if (firstname || lastname) {
+    greeting += ', ';
+    if (firstname && lastname) {
+      greeting += firstname + ' ' + lastname;
+    } else if (lastname) {
+      greeting += 'Mx. ' + lastname;
+    } else {
+      greeting += firstname;
     }
-    greeting += '!';
+  }
+  greeting += '!';
 
-    document.getElementById('greeting').textContent = greeting;
-  });
+  document.getElementById('greeting').textContent = greeting;
+});
